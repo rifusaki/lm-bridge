874d421e39592735be7f1838d4a7b2f02834230d
rifusaki <martinagmejia@gmail.com>
2023-03-29 16:24:06 -0500

    echo script completed

--- src/handlers/gpt-handler.js
+++ src/handlers/gpt-handler.js
@@ -1,15 +1,11 @@
 /*
- * Starter Project for WhatsApp Echo Bot Tutorial
- *
- * Remix this as the starting point for following the WhatsApp Echo Bot tutorial
+ * Based on Starter Project for WhatsApp Echo Bot
  *
  */
 
 "use strict";
 
-// Access token for your app
-// (copy token from DevX getting started page
-// and save it as environment variable into the .env file)
+// Access token
 const token = process.env.WHATSAPP_TOKEN;
 
 // Imports dependencies and set up http server
@@ -43,30 +39,14 @@ exports.gptHandler = async (event) => {
     }
 
     if (event.httpMethod == 'POST') {
-    }
-  }
+      let body = JSON.parse(event['body']);
+      console.log(body["object"]);
 
-  // Accepts POST requests at /webhook endpoint
-  app.post("/webhook", (req, res) => {
-    // Parse the request body from the POST
-    let body = req.body;
+      if (body["object"] && body["entry"][0]["changes"][0]["value"]["messages"][0]) {
+        let phone_number_id = body["entry"][0]["changes"][0]["value"]["metadata"]["phone_number_id"];
+        let from = body["entry"][0]["changes"][0]["value"]["messages"][0]["from"];
+        let msg_body = body["entry"][0]["changes"][0]["value"]["messages"][0]["text"]["body"];
 
-  // Check the Incoming webhook message
-  console.log(JSON.stringify(req.body, null, 2));
-
-  // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
-  if (req.body.object) {
-    if (
-      req.body.entry &&
-      req.body.entry[0].changes &&
-      req.body.entry[0].changes[0] &&
-      req.body.entry[0].changes[0].value.messages &&
-      req.body.entry[0].changes[0].value.messages[0]
-    ) {
-      let phone_number_id =
-        req.body.entry[0].changes[0].value.metadata.phone_number_id;
-        let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-        let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
         axios({
           method: "POST", // Required, HTTP method, a string, e.g. POST, GET
           url:
@@ -77,17 +57,14 @@ exports.gptHandler = async (event) => {
           data: {
             messaging_product: "whatsapp",
             to: from,
-            text: { body: "Local: " + msg_body },
+            text: { body: "AWS: " + msg_body },
           },
           headers: { "Content-Type": "application/json" },
         });
+
+        return {"statusCode": 200, "body": ""}
+      } else  {
+        return {"statusCode": 404, "body": ""}
       }
-      res.sendStatus(200);
-      callback(null, {"statusCode": 200, "body": ""})
-  } else {
-    // Return a '404 Not Found' if event is not from a WhatsApp API
-    res.sendStatus(404);
-    callback(null, {"statusCode": 404, "body": ""})
+    }
   }
-  app.close();
-});
